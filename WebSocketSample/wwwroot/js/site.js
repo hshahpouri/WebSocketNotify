871aaa574c5e320d7727b8392e4138b0769a399e
Hossein Shahpouri <hshahpouri@gmail.com>
2022-11-06 23:27:39 +0330

    Update to NET 6.0

--- WebSocketSample/wwwroot/js/site.js
+++ WebSocketSample/wwwroot/js/site.js
@@ -4,17 +4,29 @@ var ws = null;
 function socket_connect() {
 
     ws = new WebSocket(`wss://${location.hostname}:${location.port}/ws`);
-    
+
     ws.onopen = function (ev) {
         console.log("onopen", ev);
 
         $('#bConnect').addClass("d-none");
         $('#bClose').removeClass("d-none");
         $('#bSend').prop("disabled", false);
+
+        if (Notification.permission == "granted") {
+            navigator.serviceWorker.getRegistration().then((reg) => {
+                reg.showNotification("WebSocket Connected");
+            });
+        }
     };
 
     ws.onerror = function (ev) {
         console.log("onerror", ev);
+
+        if (Notification.permission == "granted") {
+            navigator.serviceWorker.getRegistration().then((reg) => {
+                reg.showNotification("WebSocket Error, See Console");
+            });
+        }
     };
 
     ws.onclose = function (ev) {
@@ -23,11 +35,32 @@ function socket_connect() {
         $('#bConnect').removeClass("d-none");
         $('#bClose').addClass("d-none");
         $('#bSend').prop("disabled", true);
+
+        if (Notification.permission == "granted") {
+            navigator.serviceWorker.getRegistration().then((reg) => {
+                reg.showNotification("WebSocket Closed");
+            });
+        }
     };
 
     ws.onmessage = function (ev) {
         console.log("onmessage", ev);
         $('#divResult>ol').append(`<li>${ev.data}</li>`);
+
+        if (Notification.permission == "granted") {
+            navigator.serviceWorker.getRegistration().then((reg) => {
+                var options = {
+                    body: ev.data,
+                    icon: '/WebSocketNotify.png',
+                    vibrate: [100, 50, 100, 50, 50],
+                    data: {
+                        dateOfArrival: Date.now(),
+                        primaryKey: 1
+                    }
+                };
+                reg.showNotification("WebSocket New Message", options);
+            });
+        }
     };
 }
 
@@ -38,4 +71,44 @@ function socket_close() {
 function socket_send() {
     ws.send($('#tbInput').val());
     $('#tbInput').val("").focus();
+
+    if (Notification.permission == "granted") {
+        navigator.serviceWorker.getRegistration().then((reg) => {
+            reg.showNotification("Message Sent Over WebSocket");
+        });
+    }
 }
+
+$(function () {
+
+
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/sw.js");
+    }
+
+    if (Notification.permission == "granted") {
+        $('#divAlert').addClass("alert-success border-success").removeClass("alert-info border-info");
+        $('#spanNotificationStatus').addClass("badge-success").removeClass("d-none").html("Notification Allowed");
+
+    }
+    else
+        if (Notification.permission == "denied") {
+            $('#divAlert').addClass("alert-warning border-warning").removeClass("alert-info border-info");
+            $('#spanNotificationStatus').addClass("badge-warning").removeClass("d-none").html("Notification Denied");
+
+        }
+        else {
+            Notification.requestPermission().then((status) => {
+
+                if (status == "granted") {
+                    $('#divAlert').addClass("alert-success border-success").removeClass("alert-info border-info");
+                    $('#spanNotificationStatus').addClass("badge-success").removeClass("d-none").html("Notification Allowed");
+                } else
+                    if (status == "denied") {
+                        $('#divAlert').addClass("alert-warning border-warning").removeClass("alert-info border-info");
+                        $('#spanNotificationStatus').addClass("badge-warning").removeClass("d-none").html("Notification Denied");
+                    }
+            });
+        }
+
+});
---
+++ WebSocketSample/wwwroot/sw.js
@@ -0,0 +1,3 @@
+﻿
+self.addEventListener("install", (ev) => {
+});
